8cd07076b0dc8a0263f6fe7741bf496708545876
AntonenkoVadym <39695174+AntonenkoVadym@users.noreply.github.com>
2020-04-07 10:05:03 +0300

    Update iframe

---
+++ IframeExample/IframeExample/Controllers/RoomsApiController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using IframeExample.Models;
+
+namespace IframeExample.Controllers
+{
+    public class RoomsApiController : ApiController
+    {
+        private GymContext db = new GymContext();
+        public IEnumerable<Room> Get()
+        {
+            return db.Rooms.ToList();
+        }
+
+        public Room Get(string id)
+        {
+            return db.Rooms.Find(Convert.ToInt32(id));
+        }
+
+        // POST api/users
+        public void Post([FromBody]Room value)
+        {
+            db.Rooms.Add(value);
+            db.SaveChanges();
+        }
+
+        // PUT api/users/5
+        public void Put(String id, [FromBody]Room value)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(value).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        // DELETE api/users/5
+        public void Delete(string id)
+        {
+            Room Room = db.Rooms.Find(Convert.ToInt32(id));
+            db.Rooms.Remove(Room);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ IframeExample/IframeExample/Models/GymContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IframeExample.Models
+{
+    public class GymContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
